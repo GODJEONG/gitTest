1d2f1d71d74ab3f4ac26eaa0a171272691b3f1fd
SJ <GODJEONG>
2024-03-06 09:44:58 +0900

    3월6일 코딩

--- day1/src/Hello.java
+++ day1/src/Hello.java
@@ -1,34 +1,37 @@
-// 20240305 신정
 
+// 20240305 신정
 import java.util.Scanner;
 
 class Hello {
 	public static void main(String args[]) {
-		Scanner sc;
-		sc = new Scanner(System.in);
-		int num = sc.nextInt();
 
-		int space = num / 2;
-		int star = 1;
+		Scanner sc = new Scanner(System.in);
+		char answer = 'Y';
+		do {
+			System.out.println("첫번째 숫자 입력");
+			int num1 = sc.nextInt();
+			System.out.println("사칙연산 입력");
+			String calcul2 = sc.next();
+			System.out.println("두번째 숫자 입력");
+			int num2 = sc.nextInt();
 
-		for (int i = 0; i < num; i++) {
-			for (int j = 0; j < space; j++) {
-				System.out.print(" ");
-			}
-			// 별 출력
-			for (int j = 0; j < star; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-
-			// 다음 행으로 이동할 때마다 공백과 별 개수 조절
-			if (i < num / 2) {
-				space--;
-				star += 2;
+			if (calcul2.equals("+")) {
+				System.out.println(num1 + num2);
+			} else if (calcul2.equals("-")) {
+				System.out.println(num1 - num2);
+			} else if (calcul2.equals("*")) {
+				System.out.println(num1 * num2);
+			} else if (calcul2.equals("/")) {
+				System.out.println((double) num1 / num2);
 			} else {
-				space++;
-				star -= 2;
+				System.out.println("잘못된 입력");
 			}
-		}
+			System.out.println("계속 진행하시겠습니까?");
+			answer = sc.next().charAt(0);
+
+		} while (answer == 'Y' | answer == 'y');
+		System.out.println("사칙연산 계산기가 종료되었습니다!");
+
+		sc.close();
 	}
 }
