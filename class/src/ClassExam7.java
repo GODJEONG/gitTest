2afa75496871c81634afaf2b94b23948579b1b3c
GODJEONG <GODJEONG>
2024-03-07 18:17:59 +0900

    추가

--- class/src/ClassExam7.java
+++ class/src/ClassExam7.java
@@ -45,6 +45,6 @@ public class ClassExam7 {
 		System.out.println(th.getA() + "+" + th.getB());
 		System.out.println(th2.getA() + "+" + th2.getB());
 		System.out.println(th3.getA() + "+" + th3.getB());
-
+		System.out.println(th3.getA() + "+" + th3.getB());
 	}
 }
