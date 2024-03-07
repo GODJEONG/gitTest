9275e09cdb2c65beb592cf709158fff8d8b1a424
GODJEONG <GODJEONG>
2024-03-07 15:25:36 +0900

    gitignore

--- class/src/ClassExam4.java
+++ class/src/ClassExam4.java
@@ -1,55 +1,88 @@
 import java.util.Scanner;
 
 public class ClassExam4 {
-	
+
 	// field -------------------------------------------------------------------
-	private int num1; 
-	private int num2; 
-	private char oper; 
-	
+	private int num1, num2;
+	private char oper;
+	private double result;
+	private char ans;
+
 	// Constructor ------------------------------------------------------------
-	public ClassExam4(int number1, int number2, char operation) {
+	public ClassExam4(int number1, int number2, char operation, char answer) {
 		num1 = number1;
 		num2 = number2;
 		oper = operation;
+		ans = answer;
 	};
-	
+
 	// main method ------------------------------------------------------------
 	public static void main(String[] args) { // method
 
-		char cn = 'Y';
 		Scanner sc = new Scanner(System.in);
-		ClassExam4 calculator = new ClassExam4(0, 0, '+');
+		// ClassExam4 calculator = new ClassExam4(1, 0, '+', 'y');
+		System.out.print("1번 숫자를 입력하세요: ");
+		int n1 = sc.nextInt();
+		System.out.print("2번 숫자를 입력하세요: ");
+		int n2 = sc.nextInt();
+		System.out.print("사칙연산을 입력하세요: ");
+		char op = sc.next().charAt(0);
+		System.out.print("사칙연산을 다시 하실건가요?(Y/y): ");
+		char an = sc.next().charAt(0);
+		ClassExam4 calculator = new ClassExam4(n1,n2 , op, an);
 
 		do {
+			/*
 			System.out.print("1번 숫자를 입력하세요: ");
-			calculator.setnum1(sc);
-			System.out.print("2번 숫자를 입력하세요: ");
-			calculator.setnum2(sc);
+			calculator.setnum1(sc.nextInt());
 			System.out.print("사칙연산을 입력하세요: ");
-			calculator.setoper(sc);
-			calculator.getResult();
+			calculator.setoper(sc.next().charAt(0));
+			System.out.print("2번 숫자를 입력하세요: ");
+			calculator.setnum2(sc.nextInt());
+			*/
 
-			System.out.print("사칙연산을 다시 하실건가요?(Y/y): ");
-			cn = sc.next().charAt(0);
-
-		} while (cn == 'Y' | cn == 'y');
-	}
-    // get, set method -----------------------------------------------------------------
+			System.out.println(calculator.getnum1() + "" + calculator.getoper() + "" + calculator.getnum2() + "=");
+			switch (calculator.getoper()) {
+			case '+':
+				calculator.result = calculator.plus(calculator.getnum1(), calculator.getnum2()); break;
+			case '-':
+				calculator.result = calculator.minus(calculator.getnum1(), calculator.getnum2()); break;
+			case '*':
+				calculator.result = calculator.multi(calculator.getnum1(), calculator.getnum2()); break;
+			case '/':
+				calculator.result = calculator.div(calculator.getnum1(), calculator.getnum2()); break;
+			}
+			System.out.print(calculator.getnum1() + "" + calculator.getoper() + "" + calculator.getnum2() + "= ");
+			System.out.println(calculator.result);
+			
 	
-	public void setnum1(Scanner sc) {
-		num1 = sc.nextInt();
+		} while (calculator.getans() == 'Y' | calculator.getans() == 'y');
+
+	}
+
+	// get, set method
+	// ----------------------------------------------------------------
+	public void setnum1(int n) {
+		num1 = n;
 		System.out.println("숫자 " + num1 + "를 입력했습니다.");
 	}
 
-	public void setnum2(Scanner sc) {
-		num2 = sc.nextInt();
+	public void setnum2(int n) {
+		num2 = n;
 		System.out.println("숫자 " + num2 + "를 입력했습니다.");
 	}
 
-	public void setoper(Scanner sc) {
-		oper = sc.next().charAt(0);
+	public void setoper(char n) {
+		oper = n;
 		System.out.println("사칙연산 " + oper + "를 입력했습니다.");
+	}
+
+	public void setans(char n) {
+		ans = n;
+	}
+
+	public void setans(double c) {
+		result = c;
 	}
 
 	public int getnum1() {
@@ -64,28 +97,27 @@ public class ClassExam4 {
 		return oper;
 	}
 
-	public void getResult() {
-		System.out.println("사칙연산 결과: " + calcul(num1, num2, oper) + "입니다.");
+	public char getans() {
+		return ans;
 	}
 
-	// 사칙연산 -----------------------------------------------------------------
+	public double getresult() {
+		return result;
+	}
 
-	public double calcul(int a, int b, char c) {
-		double answer = 0;
-		switch (c) {
-		case '+':
-			answer = a + b;
-			break;
-		case '-':
-			answer = a - b;
-			break;
-		case '*':
-			answer = a * b;
-			break;
-		case '/':
-			answer = (double) a / b;
-			break;
-		}
-		return answer;
+	public double plus(int a, int b) {
+		return result = num1 + num2;
+	}
+
+	public double minus(int a, int b) {
+		return result = num1 - num2;
+	}
+
+	public double multi(int a, int b) {
+		return result = num1 * num2;
+	}
+
+	public double div(int a, int b) {
+		return result = num1 / (double) num2;
 	}
 }
